7e19b79c3b3a5db20a0e56f2f3eb3807653b1189
Juan Oropeza <jaoc1811@gmail.com>
2024-10-06 21:00:56 +0200

    feat: added gdscript formatter addon

--- scripts/card.gd
+++ scripts/card.gd
@@ -12,13 +12,15 @@ var initial_position: Vector2
 var initial_rotation: int
 var deck_ref
 
+
 func _process(delta):
 	if is_hovering:
-		scale = Vector2(1.05,1.05)
+		scale = Vector2(1.05, 1.05)
 	else:
-		scale = Vector2(1,1)
+		scale = Vector2(1, 1)
 	if is_dragging:
 		position = get_global_mouse_position() - offset
+
 
 func _on_button_button_down() -> void:
 	if is_draggable:
@@ -27,33 +29,39 @@ func _on_button_button_down() -> void:
 		initial_position = position
 		initial_rotation = rotation_degrees
 		var tween = get_tree().create_tween()
-		tween.tween_property(self,"rotation_degrees", 0, 0.1).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "rotation_degrees", 0, 0.1).set_ease(Tween.EASE_OUT)
+
 
 func _on_button_button_up() -> void:
 	if is_dragging:
 		is_dragging = false
 		var tween = get_tree().create_tween()
 		if is_inside_deck:
-			tween.tween_property(self,"global_position", deck_ref.position, 0.1).set_ease(Tween.EASE_OUT)
+			tween.tween_property(self, "global_position", deck_ref.position, 0.1).set_ease(Tween.EASE_OUT)
 		else:
-			tween.tween_property(self,"position", initial_position, 0.1).set_ease(Tween.EASE_OUT)
-			tween.tween_property(self,"rotation_degrees", initial_rotation, 0.1).set_ease(Tween.EASE_OUT)
-	
+			tween.tween_property(self, "position", initial_position, 0.1).set_ease(Tween.EASE_OUT)
+			tween.tween_property(self, "rotation_degrees", initial_rotation, 0.1).set_ease(Tween.EASE_OUT)
+
+
 func _on_button_mouse_entered() -> void:
 	if is_draggable:
 		is_hovering = true
 
+
 func _on_button_mouse_exited() -> void:
 	is_hovering = false
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("deck"):
 		is_inside_deck = true
 		deck_ref = body
 
+
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("deck"):
 		is_inside_deck = false
+
 
 func play(player_position: int):
 	pass
--- scripts/hand.gd
+++ scripts/hand.gd
@@ -4,6 +4,7 @@ var cards: Array[Node]
 @export var cards_positions: Array[Vector2]
 @export var cards_rotations: Array[int]
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	cards = get_children()
@@ -11,11 +12,13 @@ func _ready() -> void:
 		await get_tree().create_timer(0.1).timeout
 		set_position_and_rotation(i)
 
+
 func set_position_and_rotation(card_index: int):
 	var tween = get_tree().create_tween()
 	tween.tween_property(cards[card_index], "position", cards_positions[card_index], 0.1).set_ease(Tween.EASE_OUT)
 	tween.tween_property(cards[card_index], "rotation_degrees", cards_rotations[card_index], 0.1).set_ease(Tween.EASE_OUT)
 	cards[card_index].is_draggable = true
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
