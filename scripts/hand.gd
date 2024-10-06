87c2f207a57409c5a90c99a205fb5e3e7259fddb
Juan Oropeza <jaoc1811@gmail.com>
2024-10-06 23:04:49 +0200

    feat: removed ready scened from hand script

--- scripts/hand.gd
+++ scripts/hand.gd
@@ -4,16 +4,11 @@ var cards: Array[Node]
 @export var cards_positions: Array[Vector2]
 @export var cards_rotations: Array[int]
 
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	cards = get_children()
-	for i in len(cards):
-		await get_tree().create_timer(0.1).timeout
-		set_position_and_rotation(i)
 
 
 func set_position_and_rotation(card_index: int):
+	await get_tree().create_timer(0.1).timeout
 	var tween = get_tree().create_tween()
 	var slot_position = cards_positions[card_index]
 	var slot_rotation = cards_rotations[card_index]
@@ -22,8 +17,3 @@ func set_position_and_rotation(card_index: int):
 	tween.tween_property(cards[card_index], "position", slot_position, 0.1).set_ease(Tween.EASE_OUT)
 	tween.tween_property(cards[card_index], "rotation_degrees", slot_rotation, 0.1).set_ease(Tween.EASE_OUT)
 	cards[card_index].is_draggable = true
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
