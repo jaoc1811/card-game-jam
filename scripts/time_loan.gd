4223ddfc4e6eb0673032f8c8c9f3b5e82fad6c19
Sandra <sandrav0609@gmail.com>
2024-10-07 14:52:10 -0400

    fix: time loan min passive clock validation

--- scripts/time_loan.gd
+++ scripts/time_loan.gd
@@ -6,6 +6,14 @@ func play(player_position: int):
 	## Substract 15 minutes from passive clock and add 4 hours to round points
 	#game_manager.players[player_position].passive_clock -= 15
 	#game_manager.players[player_position].round_points += 240 * game_manager.reverse_flow
+	
+	if game_manager.players[player_position].passive_clock <= 15:
+		return {
+			player_position: {
+				"passive_clock": 0,
+				"round_points": 0
+			}
+		}
 	return {
 		player_position: {
 			"passive_clock": -15,
