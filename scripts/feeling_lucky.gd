49e619e597ad05a585b74d07c12ff9ed2f37dba2
Carlos Rivero <carlosrivero96@gmail.com>
2024-10-08 13:20:14 -0400

    feat: added coin animation to feeling lucky

--- scripts/feeling_lucky.gd
+++ scripts/feeling_lucky.gd
@@ -1,6 +1,9 @@
 class_name feeling_lucky
 extends Card
 
+@onready var win: AnimatedSprite2D = $"../CoinFlip/Win"
+@onready var lose: AnimatedSprite2D = $"../CoinFlip/Lose"
+
 func play(player_position: int):
 	var heads = 1
 	var coin_value = randi() % 2 # 0 or 1
@@ -8,9 +11,27 @@ func play(player_position: int):
 	var gambling_hours_amount = 3
 	var points = hour_value * gambling_hours_amount
 	var punishment_multiplier = -1
-			
+
+	if coin_value == heads:
+		win.show()
+		win.play()
+		var duration = win.sprite_frames.get_frame_count("CoinFlipWin")/win.sprite_frames.get_animation_speed("CoinFlipWin")
+		print(duration)
+		await get_tree().create_timer(duration + 1,5).timeout
+		win.hide()
+	else:
+		lose.show()
+		lose.play()
+		var duration = win.sprite_frames.get_frame_count("CoinFlipLose")/win.sprite_frames.get_animation_speed("CoinFlipLose")
+		print(duration)
+		await get_tree().create_timer(duration + 1,5).timeout
+		win.hide()
 	return {
 		player_position: {
 			"round_points" =  points if heads == coin_value else (points * punishment_multiplier)
 		}
 	}
+
+func _ready() -> void:
+	await play(0)
+	print("Done")
