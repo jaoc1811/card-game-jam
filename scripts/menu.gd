cb518800a3de8bf1718b1dc03d4c93a88c287511
maancga <maancga@gmail.com>
2024-10-08 23:32:10 +0100

    Adds how to play scene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,6 +5,9 @@ func _on_play_pressed() -> void:
 
 func _on_sound_pressed() -> void:
 	get_tree().change_scene_to_file('res://scenes/change_sound_menu.tscn')
+	
+func _on_how_to_play_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/how_to_play.tscn')
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
