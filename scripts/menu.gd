0e03d880e0893f26fdabbefd685b1b553e50220c
maancga <maancga@gmail.com>
2024-10-08 23:08:05 +0100

    Adds  menu scenes and scripts

---
+++ scripts/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/player_selection.tscn')
+
+func _on_sound_pressed() -> void:
+	get_tree().change_scene_to_file('res://scenes/change_sound_menu.tscn')
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	
