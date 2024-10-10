dc4e42f27c9618929f887940dd959c8150523b76
Sandra <sandrav0609@gmail.com>
2024-10-10 14:52:22 -0400

    fix: volume scroll and music

--- scripts/volume_slider.gd
+++ scripts/volume_slider.gd
@@ -8,7 +8,8 @@ var bus_index: int
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
-	
+	# Set slider to current music volume
+	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+
 func _on_value_changed(value: float)->void:
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value)
-	)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
